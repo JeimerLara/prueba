f9db17826010b61063177c38b3353d5ca18636f2
JeimerLara <registroequipos2.0@gmail.com>
2026-07-31 16:20:54 -0500

    Modificacion de Reloj pm - am

--- app.js
+++ app.js
@@ -2,10 +2,15 @@ const reloj = document.getElementById('reloj');
 
 function actualizarReloj() {
     const ahora = new Date();
-    const horas = String(ahora.getHours()).padStart(2, '0');
+    const hora = ahora.getHours() % 12 || 12; // Convertir a formato de 12 horas
+    
+
+    const horas =  String(hora).padStart(2, '0');
     const minutos = String(ahora.getMinutes()).padStart(2, '0');
     const segundos = String(ahora.getSeconds()).padStart(2, '0');
-    reloj.textContent = `${horas}:${minutos}:${segundos}`;
+    const ampm = horas >= 12 ? 'a.m.' : 'p.m.'; 
+    
+    reloj.textContent = `${horas}:${minutos}:${segundos} ${ampm}`;
 }
 
 setInterval(actualizarReloj, 1000);
